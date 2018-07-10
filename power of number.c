373bec3dfffb0cf33e1cdb664cfe3d933040b4fd
yashfab29 <40981696+yashfab29@users.noreply.github.com>
2018-07-10 14:40:57 +0530

    power

---
+++ power of number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<Math.h>
+int main()
+{
+    int a,b,c;
+    printf("enter two numbers");
+    scanf("%d%d",&a,&b);
+    c=pow(a,b);
+    printf("%d",c);
+    return 0;
+}
