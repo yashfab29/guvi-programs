fa567c793fe99a71b67727d20d26c9e0d05693c5
yashfab29 <40981696+yashfab29@users.noreply.github.com>
2018-07-13 00:16:48 +0530

    factorial

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    long fact=1;
+    printf("enter a no");
+    scanf("%d",&n);
+    if(n==0)
+        printf("error");
+    else
+    {
+        for(i=1;i<=n;i++)
+            fact*=i;
+        printf("factorial of %d=%d\n",n,fact);
+    }
+
+}
