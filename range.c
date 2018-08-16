de59b42d06812b069cc6eced771a39596eb0e253
yashfab29 <40981696+yashfab29@users.noreply.github.com>
2018-08-16 12:47:55 +0530

    range

---
+++ range.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    printf("enter a number");
+    scanf("%d",&n);
+    if(n>0&&n<11)
+    {
+        printf("yes");
+    }
+    else{printf("no");}
+
+
+}
