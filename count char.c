1ee0b3cb8aeea45834bf8f5d8bf76a23a77afa59
yashfab29 <40981696+yashfab29@users.noreply.github.com>
2018-08-16 16:07:02 +0530

    count char

---
+++ count char.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    char str[30];
+    int count =0;
+    printf("enter the string");
+    gets(str);
+    int i;
+    for(i=0;str[i]!='\0';i++)
+    {
+        if(str[i]==' ')
+        {
+            continue;
+        }
+        ++count;
+    }
+    printf("%d",count);
+}
